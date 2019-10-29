d651523f008153f3ada60b729aabd7efc09b3b65
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2019-10-28 23:57:33 -0500

    1. LRU -> len(self.items) > capacity for knowing if capacity exceeded

--- 1. LRU-caching.py
+++ 1. LRU-caching.py
@@ -15,7 +15,6 @@ class LruCache(object):
     def __init__(self, capacity):
         print('Cache initiated with a capacity of:', capacity)
         self.items = {}
-        self.num_elements = 0
         self.capacity = capacity
         self.head = None
         self.tail = None
@@ -60,10 +59,8 @@ class LruCache(object):
         self.items[key] = new_node
         self.head = key
 
-        self.num_elements += 1
-
-        if self.num_elements > self.capacity:
-            print('\n-- Capacity Exceeded, deleting', self.tail)
+        if len(self.items) > self.capacity:
+            print('\n--- Capacity Exceeded, deleting', self.tail)
             tail = self.items[self.tail]  # no possible to get a KeyException
             del self.items[self.tail]
             self.tail = tail.next
@@ -78,11 +75,22 @@ if __name__ == '__main__':
     our_cache.set(4, 4)
 
     our_cache.get(1)  # returns 1
-    our_cache.get(2) # returns 2
+    our_cache.get(2)  # returns 2
     our_cache.get(9)  # returns -1 because 9 is not present in the cache
 
     our_cache.set(5, 5)
     our_cache.set(6, 6)
 
-    print(
-        our_cache.get(3))  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+    print(our_cache.get(3))  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+    print(our_cache.get(5))  # should return 5
+    print(our_cache.get(6))  # should return 6
+
+    print(our_cache.items)
+    print(our_cache.head)
+
+    our_cache.set(7, 9009)
+    our_cache.set(8, 1)
+    our_cache.set(6, 9)  # least recently used not deleted because 6 updates, does not add a new one
+    our_cache.set(3, 100)
+
+    print(our_cache.items)
